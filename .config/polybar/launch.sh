40d25db33350b2f9de4e1f5ab74560df1e8c4898
zeroliquid <maximngycat@gmail.com>
2024-09-22 04:48:13 +0300

    Multi-monitor setup, tray

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,5 +3,21 @@
 killall -q polybar
 
 SRC_DIR=$(cd ~/.config/polybar/; pwd)
-export MSG_SCRIPT=${SRC_DIR}/bar_bash.sh
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+# MSG_SCRIPT=${SRC_DIR}/bar_bash.sh polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar secondary 2>&1 | tee -a /tmp/polybar-secondary.log & disown
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) MSG_SCRIPT=${SRC_DIR}/bar_bash.sh polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+  for m in $screens; do
+    notify-send "Hello $m"
+    if [[ $primary == $m ]]; then
+        MONITOR=$m MSG_SCRIPT=${SRC_DIR}/bar_bash.sh polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+    else
+        MONITOR=$m polybar secondary 2>&1 | tee -a /tmp/polybar-secondary.log & disown
+    fi
+  done
+fi
